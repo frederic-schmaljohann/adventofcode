4d53f905637008349600984131ed789d9926e18a
Frederic Schmaljohann <frederic.schmaljohann@gmail.com>
2018-12-01 10:18:02 +0100

    Day1

---
+++ 2018/1/frequencies.scala
@@ -0,0 +1,20 @@
+def frequency(changes: List[Int]) = changes.sum
+
+def firstStateRepeated(changes: List[Int]): Unit = {
+  var statesSeen = List[Int]()
+
+  var current = 0
+
+  while (true) {
+    var changesList = changes
+    changesList.foreach(it => {
+      current = current + it
+      if (statesSeen.contains(current)) {
+        println(current)
+        throw new Exception()
+      }
+      statesSeen = current :: statesSeen
+    })
+
+  }
+}
