d348259fb833bfa51b1576af7c3a2359514c6ce2
Frederic Schmaljohann <frederic.schmaljohann@gmail.com>
2018-12-05 13:39:55 +0100

    Day 5

---
+++ 2018/5/polymer.scala
@@ -0,0 +1,25 @@
+import scala.io.Source
+
+val polymer = Source.fromFile("input").getLines.next
+react(polymer).length
+
+('a' to 'z')
+  .map(c => polymer.replaceAll(s"(?i)${c}", ""))
+  .map(react(_))
+  .minBy(_.length)
+  .length
+
+def react(polymer: String): String = {
+  var pol = polymer
+  var i = 0
+  while (i < pol.length - 1) {
+    if (pol(i) != pol(i + 1) && pol(i).toLower == pol(i + 1).toLower) {
+      pol = pol.substring(0, i) + pol.substring(i + 2)
+      i = 0
+    }
+    else {
+      i = i + 1
+    }
+  }
+  return pol
+}
