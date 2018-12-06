41a0f981788790cbb869e2496caff3c15788e1f2
Frederic Schmaljohann <frederic.schmaljohann@gmail.com>
2018-12-06 21:08:25 +0100

    improve Day 5

--- 2018/5/polymer.scala
+++ 2018/5/polymer.scala
@@ -1,6 +1,17 @@
-import scala.io.Source
+def react(polymer: String): String = {
+  var pol = polymer
+  var i = 0
+  while (i < pol.length - 1) {
+    if (pol(i) != pol(i + 1) && pol(i).toLower == pol(i + 1).toLower) {
+      pol = pol.substring(0, i) + pol.substring(i + 2)
+      i = Math.max(0, i - 1)
+    }
+    else i = i + 1
+  }
+  pol
+}
 
-val polymer = Source.fromFile("input").getLines.next
+val polymer = scala.io.Source.fromFile("input").getLines.next
 react(polymer).length
 
 ('a' to 'z')
@@ -8,16 +19,3 @@ react(polymer).length
   .map(react(_))
   .minBy(_.length)
   .length
-
-def react(polymer: String): String = {
-  var pol = polymer
-  var i = 0
-  while (i < pol.length - 1) {
-    if (pol(i) != pol(i + 1) && pol(i).toLower == pol(i + 1).toLower) {
-      pol = pol.substring(0, i) + pol.substring(i + 2)
-      i = 0
-    }
-    else i = i + 1
-  }
-  pol
-}
