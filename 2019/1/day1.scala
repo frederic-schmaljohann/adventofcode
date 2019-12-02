de60a8813d9f87882135775f284e98418507873f
Frederic Schmaljohann <frederic.schmaljohann@gmail.com>
2019-12-02 11:13:58 +0100

    use @tailrec

--- 2019/1/day1.scala
+++ 2019/1/day1.scala
@@ -5,14 +5,15 @@ val masses = (for (line <- Source.fromFile(filename).getLines) yield (line.toInt
 
 def calculateFuel(m: Int): Int = m  / 3 - 2
 
-def calculateFuelOverhead(m: Int): Int = {
+@scala.annotation.tailrec
+def calculateFuelOverhead(m: Int, acc: Int): Int = {
   val fuel0 = calculateFuel(m)
   if (fuel0 > 0)
-      fuel0 + calculateFuelOverhead(fuel0)
+    calculateFuelOverhead(fuel0, acc + fuel0)
   else
-    0
+    acc
 }
 
 println(masses.map(m => calculateFuel(m)).sum)
 
-println(masses.map(m => calculateFuelOverhead(m)).sum)
+println(masses.map(m => calculateFuelOverhead(m, 0)).sum)
