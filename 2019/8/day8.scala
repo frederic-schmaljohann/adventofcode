0c93746d47cfd826dc577ab5344cf7559107193e
Frederic Schmaljohann <frederic.schmaljohann@gmail.com>
2019-12-08 12:26:11 +0100

    cleanup day 8

--- 2019/8/day8.scala
+++ 2019/8/day8.scala
@@ -5,17 +5,17 @@ val h = 6
 
 val lines = (for (line <- Source.fromFile("input.txt").getLines()) yield line).toList
 
+//part 1
 val layer = lines(0).grouped(w * h).minBy(x => x.count(_ == '0'))
 
 layer.count(_ == '1') * layer.count(_ == '2')
 
 val allLayers = lines(0).grouped(w * h).toList
 
-val message = allLayers(0).zipWithIndex.map(it => {
+//part 2
+allLayers(0).zipWithIndex.map(it => {
   val theColorAtPos = allLayers.map(l => l(it._2)).foldRight(' ')((a, b) => if (a == '2') b else a)
   theColorAtPos
-}).grouped(w)
-
-message.foreach(line => {
+}).grouped(w).foreach(line => {
     println(line.map(it => if (it == '0') "    " else "XXXX").mkString(" "))
 })
