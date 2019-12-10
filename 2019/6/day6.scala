9868967877b8ad0f4552c9989f6ee59c8f75496e
Frederic Schmaljohann <frederic.schmaljohann@gmail.com>
2019-12-10 16:55:55 +0100

    simplify day 6

--- 2019/6/day6.scala
+++ 2019/6/day6.scala
@@ -1,87 +1,86 @@
-import scala.collection.mutable
-import scala.io.Source
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
+def createOrbits(predecessorsOnly: Boolean = true) = {
+  val orbits = mutable.Map[String, ListBuffer[String]]()
+  val lines = (for (line <- Source.fromFile("input.txt").getLines()) yield line).toList
 
-val orbits = mutable.Map[String, ListBuffer[String]]()
+  lines.foreach(l => {
+    val pair = l.split("\\)")
+    val curr = if (orbits.contains(pair(1))) {
+      orbits(pair(1))
+    }
+    else ListBuffer[String]()
+    curr += pair(0)
+    orbits.put(pair(1), curr)
 
-val lines = (for (line <- Source.fromFile("input.txt").getLines()) yield line).toList
-
-lines.foreach(l => {
-  val pair = l.split("\\)")
-
-  val curr = if (orbits.contains(pair(1))) {
-    orbits(pair(1))
-  }
-  else ListBuffer[String]()
-  curr += pair(0)
-
-  orbits.put(pair(1), curr)
-
-  val curr2 = if (orbits.contains(pair(0))) {
-    orbits(pair(0))
-  }
-  else ListBuffer[String]()
-  curr2 += pair(1)
-
-  orbits.put(pair(0), curr2)
-})
-
-val all = orbits.keySet.toList
-
-//part 1
-@tailrec
-def process(start: String, acc: Int): Int = {
-  if (start == "COM") {
-    acc
-  } else {
-    println(s"$start orbits around ${orbits(start)}")
-    process(orbits(start).head, acc + 1)
-  }
-}
-
-println(all.map(it => {
-  process(it, 0)
-}).sum)
-
-//part 2
-val distances = mutable.Map[String, Int]()
-val predecessors = mutable.Map[String, Option[String]]()
-
-def updateDistance(u: String, v: String): Unit = {
-  if (distances(u) + 1 < distances(v)) {
-    distances(v) = distances(u) + 1
-    predecessors(v) = Some(u)
-  }
-}
-
-all.foreach(s => {
-  distances.put(s, Int.MaxValue)
-  predecessors.put(s, None)
-})
-
-distances.put("YOU", 0)
-val q = all.to[ListBuffer]
-
-while (q.nonEmpty) {
-  val u = q.minBy(it => distances(it))
-  q.remove(q.indexOf(u))
-
-  val neighbours = orbits(u)
-  neighbours.foreach(neighbour => {
-    if (orbits.contains(u) && q.contains(neighbour)) {
-      updateDistance(u, neighbour)
+    if (!predecessorsOnly) {
+      val curr2 = if (orbits.contains(pair(0))) {
+        orbits(pair(0))
+      }
+      else ListBuffer[String]()
+      curr2 += pair(1)
+      orbits.put(pair(0), curr2)
     }
   })
+  orbits
 }
 
-var x = orbits("SAN").head
-var cnt = 0
-while (x != orbits("YOU").head) {
-  x = predecessors(x).get
-  println(x)
-  cnt += 1
+
+def part1(): Unit = {
+  val orbits = createOrbits(true)
+  val all = orbits.keySet.toList
+
+  @tailrec
+  def process(start: String, acc: Int): Int = {
+    if (start == "COM") {
+      acc
+    } else {
+      println(s"$start orbits around ${orbits(start)}")
+      process(orbits(start).head, acc + 1)
+    }
+  }
+
+  println(all.map(it => {
+    process(it, 0)
+  }).sum)
 }
 
-println(cnt)
+case class Node(label: String, predecessor: Node)
+def bfs(g: mutable.Map[String, ListBuffer[String]], start: Node, dest: String): Node = {
+  val orbitsSeen = mutable.Map[String, Boolean]()
+  val q = mutable.Queue[Node]()
+  orbitsSeen.put(start.label, true)
+  q.enqueue(start)
+
+  while (q.nonEmpty) {
+    val v = q.dequeue()
+    if (v.label == dest) {
+      return v
+    }
+    g(v.label).foreach(w => {
+      if (orbitsSeen.get(w).isEmpty) {
+        orbitsSeen.put(w, true)
+        val wN = Node(w, v)
+        q.enqueue(wN)
+      }
+    })
+  }
+  null
+}
+
+def part2(): Unit = {
+  var counter = 0
+  val orbits = createOrbits(false)
+  var p = bfs(orbits, Node("YOU", null), "SAN")
+  while (p.label != orbits("YOU").head) {
+    p = p.predecessor
+    counter += 1
+  }
+  println(counter)
+}
+
+part1()
+part2()
